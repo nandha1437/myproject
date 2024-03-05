c34e07d1b5e2cb9356f8a89598c889eae29e4c4c
nandha <nandagopald1437@gmail.com>
2024-03-05 10:45:55 +0530

    first

---
+++ calander.py
@@ -0,0 +1,12 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import calender
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import calender
+ModuleNotFoundError: No module named 'calender'
+>>> import calendar
+>>> year=12
+>>> year=2022
+>>> month=10
+>>> print(calendar.month(year,month)
